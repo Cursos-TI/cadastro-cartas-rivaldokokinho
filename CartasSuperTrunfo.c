c31cc7d57266142d782042c6df8b1abcb3f1fe6f
rivaldokokinho <128857854+rivaldokokinho@users.noreply.github.com>
2026-03-12 20:06:29 +0000

    etapa de comparacao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,12 +2,12 @@
 
 int main() {
 // Área para definição das variáveis para armazenar as propriedades das cidades
-signed long int populacao, populacao1;
+unsigned long int populacao, populacao1;
 int numero_de_pontos_turisticos,  numero_de_pontos_turisticos1;
 char estado[50], estado1[50], codigo_da_carta[50], codigo_da_carta1[50], nome_da_cidade[50], nome_da_cidade1[50];
 float area_em_km², area_em_km²1, pib, pib1;
 float pib_per_capital, pib_per_capital1, densidade_populacional, densidade_populacional1, super_poder, super_poder1;
-
+int resultado;
 // *** Área para entrada de dados carta 1
 printf("Cadastro carta 1 \n");
 
@@ -21,7 +21,7 @@ printf("Digite o nome da cidade: \n");
 scanf("%s", &nome_da_cidade);
 
 printf("Qual a população: \n");
-scanf("%d", &populacao);
+scanf("%lu", &populacao);
 
 printf("Qual o numero de pontos turistico: \n");
 scanf("%d", &numero_de_pontos_turisticos);
@@ -57,7 +57,7 @@ printf("Digite o nome da cidade: \n");
 scanf("%s", &nome_da_cidade1);
 
 printf("Qual a população: \n");
-scanf("%d", &populacao1);
+scanf("%lu", &populacao1);
 
 printf("Qual o numero de pontos turistico: \n");
 scanf("%d", &numero_de_pontos_turisticos1);
@@ -80,6 +80,16 @@ printf("Pib per capital: %f\n", pib_per_capital1);
 
 super_poder1 = (float) + area_em_km²1 + pib1 + numero_de_pontos_turisticos1 + pib_per_capital1 + densidade_populacional1;
 printf("Super poder: %f\n ", super_poder1);
+
+//           COMPARACAO DE CARTAS                 //
+populacao1 > populacao;
+numero_de_pontos_turisticos1 > numero_de_pontos_turisticos;
+area_em_km²1 > area_em_km²;
+pib1 > pib;
+densidade_populacional1 < densidade_populacional;
+pib_per_capital1 > pib_per_capital;
+super_poder1 > super_poder;
+
 
 return 0;
 } 
