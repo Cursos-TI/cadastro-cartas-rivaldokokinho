40d8589cb3013ea3b3b7e2e074bafc8ba74bfedc
rivaldokokinho <128857854+rivaldokokinho@users.noreply.github.com>
2026-03-12 00:28:36 +0000

    avançando no nivel mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 
 int main() {
-  /*
-soma (+)
-subtracao(-)
-multiplicacao(*)
-divisao(/)
-*/
-  // Área para definição das variáveis para armazenar as propriedades das cidades
-
-  int populacao, numero_de_pontos_turisticos;
+// Área para definição das variáveis para armazenar as propriedades das cidades
+signed long int populacao;
+int numero_de_pontos_turisticos;
 char estado[50], codigo_da_carta[50], nome_da_cidade[50];
 float area_em_km², pib;
-float divisao;
+float pib_per_capital, densidade_populacional, super_poder;
 
 // *** Área para entrada de dados carta 1
 printf("Cadastro carta 1 \n");
@@ -42,15 +36,17 @@ printf("Codigo da carta: %s\n  Estado: %s\n  Cidade: %s\n", codigo_da_carta, est
 printf("População: %d\n  Pontos turisticos: %d\n  Pib: %f\n  Area km²: %f\n", populacao, numero_de_pontos_turisticos, pib, area_em_km²);
 
 // *** calculo ***//
-divisao = populacao / area_em_km²;
-printf("A densidade populacional e: %f\n", divisao);
+densidade_populacional = populacao / area_em_km²;
+printf("Densidade populacional: %f\n", densidade_populacional);
 
-divisao = pib / populacao;
-printf("Pib per capital e: %f\n",divisao);
+pib_per_capital = pib / populacao;
+printf("Pib per capital: %f\n", pib_per_capital);
+
+super_poder = (float) + area_em_km² + pib + numero_de_pontos_turisticos + pib_per_capital + densidade_populacional;
+printf("Super poder: %f\n ", super_poder);
 
 // *** Área para entrada de dados carta 2
 printf("Cadastro carta 2\n");
-
 printf("Digite o codigo da carta: \n");
 scanf("%s", &codigo_da_carta);
 
@@ -76,11 +72,14 @@ printf("Codigo da carta: %s\n  Estado: %s\n  Cidade: %s\n", codigo_da_carta, est
 printf("População: %d\n  Pontos turisticos: %d\n  Pib: %f\n  Area km²: %f\n", populacao, numero_de_pontos_turisticos, pib, area_em_km²);
 
 // *** calculo ***//
-divisao = populacao / area_em_km²;
-printf("A densidade populacional e: %f\n", divisao);
+densidade_populacional = populacao / area_em_km²;
+printf("Densidade populacional: %f\n", densidade_populacional);
 
-divisao = pib / populacao;
-printf("Pib per capital e: %f\n",divisao);
+pib_per_capital = pib / populacao;
+printf("Pib per capital: %f\n", pib_per_capital);
+
+super_poder = (float) + area_em_km² + pib + numero_de_pontos_turisticos + pib_per_capital + densidade_populacional;
+printf("Super poder: %f\n ", super_poder);
 
 return 0;
 } 
